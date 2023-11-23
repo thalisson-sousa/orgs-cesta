f3cac0fe5bbc119ad4ac33e151ae85a3045535a5
Thalisson <thalissonpop91@gmail.com>
2023-11-23 03:24:00 -0300

    app-react-native

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet, View } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if(!fontCarregada) {
+    return <View />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/telas/Cesta.js
@@ -0,0 +1,79 @@
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
+
+import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
+
+const width = Dimensions.get('screen').width;
+
+export default function Cesta() {
+    return <>
+        <Image source={topo} style={estilos.topo} />
+        <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta de Verduras</Text>
+
+            <View style={estilos.fazenda}>
+            <Image source={logo} style={estilos.imagemFazenda} />
+            <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+            </View>
+
+            <Text  style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
+            <Text  style={estilos.preco}>R$40,00</Text>
+        </View>
+        </>
+}
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+        fontFamily: "Montserrat"
+    },
+    titulo: {
+        width: '100%',
+        position: 'absolute',
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 16,
+        lineHeight: 26,
+        fontWeight: 'bold',
+        padding: 16,
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontFamily: "MontserratBold",
+    },
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32,
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: "MontserratRegular",
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
+    }
+});
